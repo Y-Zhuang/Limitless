ca9f34a58ccc5a3a7ff1e6e07fec8aa5906d2b3f
Y-Zhuang <648903224@qq.com>
2020-03-21 07:11:25 +0800

    修改service层

--- src/com/zhuang/limitless/service/UserService.java
+++ src/com/zhuang/limitless/service/UserService.java
@@ -6,21 +6,25 @@ import com.zhuang.limitless.util.MD5Util;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("userService")
-public class UserService extends BaseDaoImpl<User> {
+@Service
+public class UserService {
+
+    @Autowired
+    private BaseDaoImpl<User> baseDao;
 
     private DetachedCriteria detachedCriteria;
 
     public List<User> getUserAll(){
-        return getALL();
+        return baseDao.getALL(User.class);
     }
 
     public User getUserById(int id) {
-        return getEntityById(id);
+        return baseDao.getEntityById(User.class, id);
     }
 
     public boolean judgeNameBeing(String userName) {
@@ -33,7 +37,7 @@ public class UserService extends BaseDaoImpl<User> {
     public User getUserByName(String userName) {
         detachedCriteria = DetachedCriteria.forClass(User.class);
         detachedCriteria.add(Restrictions.eq("userName", userName));
-        return getEntitySingle(detachedCriteria);
+        return baseDao.getEntitySingle(detachedCriteria);
     }
 
     public boolean loginUser(String userName, String userPassword) {
@@ -48,29 +52,29 @@ public class UserService extends BaseDaoImpl<User> {
 
     public boolean regUser(String userName, String userPassword, byte[] userPicture) {
         User user = new User(userName, MD5Util.MD5(userPassword), userPicture);
-        return insertEntity(user);
+        return baseDao.insertEntity(user);
     }
 
     public Boolean updateUser(Integer id, String userName, String userPassword, byte[] userPicture) {
         User user = new User(userName, userPassword, userPicture);
         user.setId(id);
-        return updateEntity(user);
+        return baseDao.updateEntity(user);
     }
 
     public Boolean deleteUser(Integer id){
-        return deleteEntity(id);
+        return baseDao.deleteEntity(User.class ,id);
     }
 
     public List<User> searchUser(String searchText){
         detachedCriteria = DetachedCriteria.forClass(User.class);
         detachedCriteria.add(Restrictions.like("userName","%" + searchText + "%"));
-        return queryEntityMany(detachedCriteria);
+        return baseDao.queryEntityMany(detachedCriteria);
     }
 
     public String getUserNameById(int id){
         detachedCriteria = DetachedCriteria.forClass(User.class);
         detachedCriteria.setProjection(Projections.property("userName"));
         detachedCriteria.add(Restrictions.eq("id", id));
-        return queryEntityField(detachedCriteria).get(0).toString();
+        return baseDao.queryEntityField(detachedCriteria).get(0).toString();
     }
 }
